1f1284ee790932154fadf568b4f70d770f6b2c5e
kwonsungja <4gg4g4h4@gmail.com>
2024-12-17 16:06:12 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -9,3 +9,8 @@ image_url = "https://example.com/cute-character.png"  # 여기에 캐릭터 이�
 st.image(image_url, caption="Your friendly mascot!", width=300)
 
 st.write("This cute character is here to cheer you on as you learn and grow! 🌟")
+
+uploaded_file = st.file_uploader("Upload a cute character image", type=["png", "jpg", "jpeg"])
+if uploaded_file is not None:
+    st.image(uploaded_file, caption="Your friendly mascot!", width=300)
+
