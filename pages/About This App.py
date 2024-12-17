db91b0495fffa0e41592d3d97c4a168b095693b4
kwonsungja <4gg4g4h4@gmail.com>
2024-12-17 17:07:38 +0900

    Update About This App.py

--- pages/About This App.py
+++ pages/About This App.py
@@ -10,4 +10,10 @@ st.markdown("➰ Since Dec. 17, 2024.")
 st.write("To improve about basic rules for forming regular plural. First add -s to the singular noun, Second add -es to nouns ending in -s, -ss, -sh, -ch, -x, -z, or -o. Last if the -y is preceded by a consonant, change -y to -ies. Students sometimes may not know how to use -s, -es, or -ies after singular nouns. By learning these rules, they can resolve noun agreement issues and improve their understanding of nouns. Therefore, by utilizing this app, learners can be encouraged to participate, boosting their motivation and interest in learning English.")
 
 
-
+st.markdown("""
+- 📝 **Understand Noun rules**  
+- 📚 **Try it repeatedly**  
+- 💪 **Work out for 10 minutes every day**  
+- 🍎 **Remember your learning**  
+- 🌟 **Reflect on today's achievements**  
+""")
