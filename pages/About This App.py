4cf90dff2252dbae601dbd0ca761dcb7018b6870
kwonsungja <4gg4g4h4@gmail.com>
2024-12-17 16:48:48 +0900

    Update About This App.py

--- pages/About This App.py
+++ pages/About This App.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
+st.markdown("Hello, World! :earth_americas:")
+st.markdown("Keep up the good work! :thumbsup: :smile:")
+st.markdown("practice with this app! :tada: :rocket: :heart:")
+
 st.title("About This App")
 st.markdown("➰ URL: ksj24firstapp")
 st.write("To improve about basic rules for forming regular plural. First add -s to the singular noun, Second add -es to nouns ending in -s, -ss, -sh, -ch, -x, -z, or -o. Last if the -y is preceded by a consonant, change -y to -ies. Students sometimes may not know how to use -s, -es, or -ies after singular nouns. By learning these rules, they can resolve noun agreement issues and improve their understanding of nouns. Therefore, by utilizing this app, learners can be encouraged to participate, boosting their motivation and interest in learning English.")
 
-st.markdown("Hello, World! :earth_americas:")
-st.markdown("Keep up the good work! :thumbsup: :smile:")
-st.markdown("Celebrate with us! :tada: :rocket: :heart:")
+
 
